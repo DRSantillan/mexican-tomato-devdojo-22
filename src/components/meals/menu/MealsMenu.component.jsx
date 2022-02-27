32a9116abc82a9894fbcdbc1690c3fbdd83cdc26
Dean <dean@deansantillan.com>
2022-02-27 13:43:59 +1000

    update(): add a firebase database and created data for the menu. Added logic to pull data into application with useEffect and useState.

--- src/components/meals/menu/MealsMenu.component.jsx
+++ src/components/meals/menu/MealsMenu.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import css from './MealsMenu.styles.module.scss';
 import Card from '../../ui/card/Card.component';
 import MealItem from '../item/MealItem.component';
@@ -6,7 +6,34 @@ import MealItem from '../item/MealItem.component';
 import { MENU_DB } from '../../../data/menu.db';
 
 const MealsMenu = () => {
-	const menuList = MENU_DB.map(menuItem => (
+	const [menuItems, setMenuItems] = useState([])
+	
+	useEffect(() => {
+		const getMenuItems = async () => {
+			const menuItems = [];
+			const response = await fetch(
+				'https://mexican-tomatoes-devdojo-22-default-rtdb.asia-southeast1.firebasedatabase.app/menu.json'
+			);
+			const data = await response.json();
+			
+			for (const key in data) {
+				const menuItem = {
+					id: key,
+					name: data[key].name,
+					description: data[key].description,
+					price: data[key].price,
+				};
+				menuItems.push(menuItem)
+				
+			}
+
+			setMenuItems(menuItems)
+		};
+
+		getMenuItems();
+	}, []);
+
+	const menuList = menuItems.map(menuItem => (
 		<MealItem key={menuItem.id} {...menuItem} />
 	));
 
